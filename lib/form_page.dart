aa14a7633d09af02f7c0449413533e0905ded91e
bath0ry <plylegacy@gmail.com>
2023-04-11 21:03:47 -0300

    add controllers to text fields/add validator

--- lib/form_page.dart
+++ lib/form_page.dart
@@ -11,6 +11,13 @@ class FormPage extends StatefulWidget {
 
 class _FormPageState extends State<FormPage> {
   TextEditingController controllerName = TextEditingController();
+  TextEditingController controllerEquipament = TextEditingController();
+  TextEditingController controllerSigla = TextEditingController();
+  TextEditingController controllerHorimetroInicio = TextEditingController();
+  TextEditingController controllerHorimetroFinal = TextEditingController();
+  TextEditingController controllerHorarioIni = TextEditingController();
+  TextEditingController controllerHorarioFinal = TextEditingController();
+  TextEditingController controllerDate = TextEditingController();
   final _formKey = GlobalKey<FormState>();
   bool valueValidator(value) {
     if (value != null && value.isEmpty) {
@@ -19,6 +26,8 @@ class _FormPageState extends State<FormPage> {
     return false;
   }
 
+  int lenghtTime = 5;
+
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -26,6 +35,28 @@ class _FormPageState extends State<FormPage> {
       child: SingleChildScrollView(
           child: Column(
         children: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextFormField(
+                keyboardType: TextInputType.datetime,
+                maxLength: 10,
+                validator: (value) {
+                  if (valueValidator(value)) {
+                    return 'Não há informações suficientes';
+                  }
+                  return null;
+                },
+                onChanged: ((value) {}),
+                controller: controllerDate,
+                style: TextStyle(fontSize: 19, color: Colors.black),
+                decoration: InputDecoration(
+                  hintStyle: TextStyle(color: Color.fromARGB(255, 73, 73, 73)),
+                  hintText: 'Data',
+                  filled: true,
+                  hintMaxLines: 2,
+                  fillColor: Colors.white,
+                )),
+          ),
           Padding(
             padding: const EdgeInsets.all(12.0),
             child: TextFormField(
@@ -41,24 +72,173 @@ class _FormPageState extends State<FormPage> {
                 decoration: InputDecoration(
                     hintStyle:
                         TextStyle(color: Color.fromARGB(255, 73, 73, 73)),
-                    hintText: 'Insira seu nome',
+                    hintText: 'Operador/Motorista',
                     filled: true,
                     hintMaxLines: 2,
                     fillColor: Colors.white,
                     border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(20)))),
           ),
-          TextButton(
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {}
-              },
-              child: Text(
-                'ENVIAR',
-                style: TextStyle(color: Colors.white),
-              ),
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(
-                      Color.fromARGB(255, 10, 74, 126))))
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextFormField(
+                validator: (value) {
+                  if (valueValidator(value)) {
+                    return 'Não há informações suficientes';
+                  }
+                  return null;
+                },
+                onChanged: ((value) {}),
+                controller: controllerEquipament,
+                style: TextStyle(fontSize: 24, color: Colors.black),
+                decoration: InputDecoration(
+                    hintStyle:
+                        TextStyle(color: Color.fromARGB(255, 73, 73, 73)),
+                    hintText: 'Equipamento',
+                    filled: true,
+                    hintMaxLines: 2,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20)))),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextFormField(
+                validator: (value) {
+                  if (valueValidator(value)) {
+                    return 'Não há informações suficientes';
+                  }
+                  return null;
+                },
+                onChanged: ((value) {}),
+                controller: controllerSigla,
+                style: TextStyle(fontSize: 24, color: Colors.black),
+                decoration: InputDecoration(
+                    hintStyle:
+                        TextStyle(color: Color.fromARGB(255, 73, 73, 73)),
+                    hintText: 'Sigla do Consórcio',
+                    filled: true,
+                    hintMaxLines: 2,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20)))),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextFormField(
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (valueValidator(value)) {
+                    return 'Não há informações suficientes';
+                  }
+                  return null;
+                },
+                onChanged: ((value) {}),
+                controller: controllerHorimetroInicio,
+                style: TextStyle(fontSize: 24, color: Colors.black),
+                decoration: InputDecoration(
+                    hintStyle:
+                        TextStyle(color: Color.fromARGB(255, 73, 73, 73)),
+                    hintText: 'Horímetro Inicial',
+                    filled: true,
+                    hintMaxLines: 2,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20)))),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextFormField(
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (valueValidator(value)) {
+                    return 'Não há informações suficientes';
+                  }
+                  return null;
+                },
+                onChanged: ((value) {}),
+                controller: controllerHorimetroFinal,
+                style: TextStyle(fontSize: 24, color: Colors.black),
+                decoration: InputDecoration(
+                    hintStyle:
+                        TextStyle(color: Color.fromARGB(255, 73, 73, 73)),
+                    hintText: 'Horímetro Final',
+                    filled: true,
+                    hintMaxLines: 2,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20)))),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextFormField(
+                maxLength: 5,
+                keyboardType: TextInputType.datetime,
+                validator: (value) {
+                  if (valueValidator(value)) {
+                    return 'Não há informações suficientes';
+                  }
+                  return null;
+                },
+                onChanged: ((value) {}),
+                controller: controllerHorarioIni,
+                style: TextStyle(fontSize: 24, color: Colors.black),
+                decoration: InputDecoration(
+                    hintStyle:
+                        TextStyle(color: Color.fromARGB(255, 73, 73, 73)),
+                    hintText: 'Horário Inicial',
+                    filled: true,
+                    hintMaxLines: 2,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20)))),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextFormField(
+                maxLength: 5,
+                keyboardType: TextInputType.datetime,
+                validator: (value) {
+                  if (valueValidator(value)) {
+                    return 'Não há informações suficientes';
+                  }
+                  return null;
+                },
+                onChanged: ((value) {}),
+                controller: controllerHorarioFinal,
+                style: TextStyle(fontSize: 24, color: Colors.black),
+                decoration: InputDecoration(
+                    hintStyle:
+                        TextStyle(color: Color.fromARGB(255, 73, 73, 73)),
+                    hintText: 'Horário Final',
+                    filled: true,
+                    hintMaxLines: 2,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20)))),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              width: 150,
+              height: 50,
+              child: TextButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {}
+                  },
+                  child: Text(
+                    'ENVIAR',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  style: ButtonStyle(
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18))),
+                      side: MaterialStateProperty.all(
+                          BorderSide(width: 2, color: Colors.black)),
+                      backgroundColor: MaterialStateProperty.all(
+                          Color.fromARGB(255, 10, 74, 126)))),
+            ),
+          )
         ],
       )),
     );
